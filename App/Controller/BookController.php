c73db69eb89bb5604317b32bda93afc996dc9505
Melissa Benard <benardmeli@yahoo.fr>
2023-03-07 12:57:36 +0100

    Create a Book entity.

--- App/Controller/BookController.php
+++ App/Controller/BookController.php
@@ -7,4 +7,27 @@ namespace App\Controller;
 class BookController
 {
 
+    public function route(): void {
+        try {
+            if(isset($_GET['action'])) {
+                switch ($_GET['action']) {
+                    case 'show':
+                        $this->about();
+                        break;
+                    case 'list':
+                        $this->home();
+                        break;
+                    default:
+                        throw new \Exception("Cette action n'existe pas.");
+                }
+            } else {
+                throw new \Exception("Aucune action détectée.");
+            }
+        } catch (\Exception $e) {
+            // Render the error page with the message of the error
+            $this->render('errors/default', [
+                'error' => $e->getMessage(),
+            ]);
+        }
+    }
 }
--- App/Entity/Book.php
+++ App/Entity/Book.php
@@ -1,7 +1,49 @@
 <?php
 
+namespace App\Entity;
 
 class Book
 {
+    protected ?int $id = null;
+    protected string $title;
+    protected string $description;
+
+
+    /**   Getters & setters   */
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+
 
 }
